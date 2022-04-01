bad6edb455fc7ded658728036cf4ff9ff4e619b5
Matt <matthias.cartel@gmail.com>
2022-04-01 19:21:49 +0200

    refactor: first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const TelegramBot = require("node-telegram-bot-api");
+const TempMail = require("node-temp-mail");
+const { v4: uuidv4 } = require("uuid");
+const token = "";
+
+let account;
+let address;
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, { polling: true });
+
+let isEmailCreated = false;
+// Matches "/echo [whatever]"
+bot.onText(/\/generate/, (msg) => {
+  const chatId = msg.chat.id;
+  const uuid = uuidv4();
+  account = new TempMail(uuid);
+
+  console.log(address);
+  isEmailCreated = true;
+
+  bot.sendMessage(
+    chatId,
+    `Temporary email created : ${account.getAddress().address}`
+  );
+  // send back the matched "whatever" to the chat
+  // bot.sendMessage(chatId, ` For retrivied messages type /retrieve,`);
+});
+
+bot.onText(/\/retrieve/, (msg) => {
+  const chatId = msg.chat.id;
+
+  if (isEmailCreated) {
+    account.fetchEmails(function (err, body) {
+      console.log(body);
+      bot.sendMessage(
+        chatId,
+        `Mail content : ${JSON.stringify(body.messages)}`
+      );
+    });
+  }
+});
+
+bot.on("polling_error", console.log);
